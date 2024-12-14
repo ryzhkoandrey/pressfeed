df7c50067f03b9243a0646f35698c9461d09d7cd
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-14 10:36:37 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// ===============  ===============
+// =============== MENU ===============
+
+const menuToggler = document.querySelector('.header__menu-toggler');
+const menu = document.querySelector('.menu');
+
+menuToggler.onclick = toggleMenu;
+
+function toggleMenu() {
+    menuToggler.classList.toggle('header__menu-toggler--active');
+    menu.classList.toggle('menu--active');
+    document.body.classList.toggle('no-scroll');
+};
